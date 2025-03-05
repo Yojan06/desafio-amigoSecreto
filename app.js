6d37c7ac843b2302c0bec15fce775cc31f3f5b83
Yojan06 <yojansanchez1998@gmail.com>
2025-03-05 11:53:02 -0400

    Implementa funcion Actualizar amigos

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombresAmigos = [];
 
+
 function agregarAmigo(){
    let nombre = document.querySelector(".input-name").value;
 
@@ -11,4 +12,23 @@ function agregarAmigo(){
    }
    console.log(nombresAmigos);
    document.getElementById("amigo").value = "";
+
+   actualizarListaAmigos();
+  
 }
+
+
+function actualizarListaAmigos(){
+   listaDeAmigos = document.querySelector(".name-list");
+   listaDeAmigos.innerHTML= "";
+
+   for (let i = 0; i < nombresAmigos.length; i++) {
+      let elemento = document.createElement("li");
+      elemento.textContent = nombresAmigos[i];
+     // console.log("Eres" + elemento[i]);
+      listaDeAmigos.appendChild(elemento);
+      
+   }
+
+}
+
